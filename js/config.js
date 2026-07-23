34b464f0a73982c7d4dd05a9ce6713b31e490f9b
BigPapaTilou <matteoportais@gmail.com>
2026-07-23 12:23:37 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,29 @@
+const CONFIG = {
+
+    tickerSpeed: 55,
+
+    refreshRate: 30000,
+
+    alertDuration:5000,
+
+
+    showTeamLogos:true,
+
+    showLeagueIcons:true,
+
+    enableAlerts:true,
+
+    tlsPulse:true,
+
+
+    sports:[
+        "NFL",
+        "NCAA",
+        "NBA",
+        "MLB",
+        "PGA",
+        "EPL",
+        "LIGUE1"
+    ]
+
+};
