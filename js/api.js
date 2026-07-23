9ba57a858bd0a8bfa7d35e41c6af58f25fbe1594
BigPapaTilou <matteoportais@gmail.com>
2026-07-23 23:16:32 +0200

    Update api.js

--- js/api.js
+++ js/api.js
@@ -255,7 +255,7 @@ const response = await fetch(
 
 const data = await response.json();
 
-console.log("PGA EVENT", pga.events[0]);    
+console.log("PGA EVENT", data.events[0]);    
 
 return data.events || null;
 
