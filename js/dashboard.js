96c33aa28aaf94ceb9625988ad516119906af99f
BigPapaTilou <matteoportais@gmail.com>
2026-07-24 15:16:16 +0200

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -7,9 +7,60 @@
 
 function updateDashboard(){
 
+
     console.log("Dashboard update");
 
+
+
+    const nflFantasyPlayers = [
+
+        "Josh Allen",
+        "Saquon Barkley",
+        "Ja'Marr Chase",
+        "Lamar Jackson",
+        "Christian McCaffrey"
+
+    ];
+
+
+
+
+    const container = 
+    document.getElementById(
+        "nfl-fantasy-list"
+    );
+
+
+
+    if(!container){
+
+        return;
+
+    }
+
+
+
+
+    container.innerHTML = "";
+
+
+
+    nflFantasyPlayers.forEach(
+        (player,index)=>{
+
+
+            container.innerHTML +=
+
+            `
+            <div>
+            ${index+1}. ${player}
+            </div>
+            `;
+
+
+        }
+
+    );
+
+
 }
-
-
-updateDashboard();
