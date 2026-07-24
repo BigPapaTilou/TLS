9829df0539071fe07474a5f4a0c21294fe2b9bbd
BigPapaTilou <matteoportais@gmail.com>
2026-07-24 22:40:01 +0200

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -147,7 +147,7 @@ async function loadLigue1Table(){
 
 }
 
-function updateDashboard(){
+async function updateDashboard(){
   console.log("Dashboard update");  
  
  
@@ -204,57 +204,302 @@ function updateDashboard(){
 
 
 
-    const mlbBattingPlayers = [
+    const mlbContainer =
+document.getElementById(
+    "mlb-batting-list"
+);
 
-        "Aaron Judge",
-        "Shohei Ohtani",
-        "Juan Soto",
-        "Mookie Betts",
-        "Ronald Acuña Jr."
+
+if(mlbContainer){
+
+
+    const mlbBattingPlayers =
+    await getMLBAvgLeaders();
+
+
+    mlbContainer.innerHTML = "";
+
+
+    mlbBattingPlayers
+    .slice(0,5)
+    .forEach((player,index)=>{
+
+
+        mlbContainer.innerHTML +=
+
+        `
+        <div>
+            ${index+1}. ${player.name}
+            <span>${player.avg}</span>
+        </div>
+        `;
+
+
+    });
+
+
+}
+
+
+}
+
+
+/*
+==============================
+ TLS SPORTS DASHBOARD
+==============================
+*/
+
+async function loadEPLTable(){
+
+
+    const epl = await fetchEPLStandings();
+
+
+    const container =
+    document.getElementById(
+        "epl-table-list"
+    );
+
+
+    if(!container){
+
+        return;
+
+    }
+
+ 
+    container.innerHTML = "";
+
+
+    const teams =
+    epl.children[0]
+    .standings
+    .entries
+    .slice(0,5);
+
+
+
+    teams.forEach(
+(team,index)=>{
+
+
+    const points =
+    team.stats.find(
+        stat => stat.name === "points"
+    )?.value;
+
+
+    container.innerHTML +=
+
+`
+<div class="epl-team-row">
+
+    <img 
+    src="${team.team.logos[0].href}"
+    class="team-logo"
+    >
+
+    <span>
+    ${team.team.name}
+    </span>
+
+    <strong>
+    ${points}
+    </strong>
+
+</div>
+`;
+
+
+}
+);
+
+
+}
+async function loadLigue1Table(){
+
+    const ligue1 = await fetchLigue1Standings();
+
+    console.log(
+        "LIGUE 1 DASHBOARD TEST",
+        ligue1
+    );
+
+
+    const teams =
+    ligue1.children[0]
+    .standings
+    .entries
+    .slice(0,5);
+
+
+    console.log(
+        "LIGUE 1 TEAMS",
+        teams
+    );
+
+
+    const container =
+    document.getElementById(
+        "ligue1-table-list"
+    );
+
+
+    if(!container){
+
+        return;
+
+    }
+
+
+    container.innerHTML = "";
+
+
+    teams.forEach(
+    (team,index)=>{
+
+
+        const points =
+        team.stats.find(
+            stat => stat.name === "points"
+        )?.value;
+
+
+        container.innerHTML +=
+
+        `
+        <div class="epl-team-row">
+
+            <img
+            src="${team.team.logos[0].href}"
+            class="team-logo"
+            >
+
+            <span>
+            ${team.team.name}
+            </span>
+
+            <strong>
+            ${points}
+            </strong>
+
+        </div>
+        `;
+
+
+    });
+
+
+}
+
+async function updateDashboard(){
+  console.log("Dashboard update");  
+ 
+ 
+ const nflFantasyPlayers = [
+
+        "Josh Allen",
+        "Saquon Barkley",
+        "Ja'Marr Chase",
+        "Lamar Jackson",
+        "Christian McCaffrey"
 
     ];
 
 
 
 
-    const mlbContainer =
+    const container = 
     document.getElementById(
-        "mlb-batting-list"
+        "nfl-fantasy-list"
     );
 
 
 
-        if(mlbContainer){
+    if(!container){
 
-
-        mlbContainer.innerHTML = "";
-
-
-
-        mlbBattingPlayers.forEach(
-            (player,index)=>{
-
-
-                mlbContainer.innerHTML +=
-
-                `
-                <div>
-                ${index+1}. ${player}
-                </div>
-                `;
-
-
-            }
-
-        );
-
+        return;
 
     }
+
+
+
+
+    container.innerHTML = "";
+
+
+
+        nflFantasyPlayers.forEach(
+        (player,index)=>{
+
+
+            container.innerHTML +=
+
+            `
+            <div>
+            ${index+1}. ${player}
+            </div>
+            `;
+
+
+        }
+
+    );
+
+
+
+
+    const mlbContainer =
+document.getElementById(
+    "mlb-batting-list"
+);
+
+
+if(mlbContainer){
+
+
+    const mlbBattingPlayers =
+    await getMLBAvgLeaders();
+
+
+    mlbContainer.innerHTML = "";
+
+
+    mlbBattingPlayers
+    .slice(0,5)
+    .forEach((player,index)=>{
+
+
+        mlbContainer.innerHTML +=
+
+        `
+        <div>
+            ${index+1}. ${player.name}
+            <span>${player.avg}</span>
+        </div>
+        `;
+
+
+    });
 
 
 }
 
 
-updateDashboard();
-loadEPLTable();
-loadLigue1Table();
+}
+
+
+async function initDashboard(){
+
+    await updateDashboard();
+
+    await loadEPLTable();
+
+    await loadLigue1Table();
+
+}
+
+
+initDashboard();
+
